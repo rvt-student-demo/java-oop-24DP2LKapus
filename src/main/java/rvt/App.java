ff7e33ec2eacd6143b023f9db4731305f00da4d5
24DP2LKapus <a242772lk@rvt.lv>
2026-05-28 14:28:27 +0300

    SQLite

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,21 @@
 package rvt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) {
-        
+        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db"); 
+        Statement statement = connection.createStatement()){
+            String sql = "CREATE TABLE todo" + "(id INTEGER PRIMARY KEY, task TEXT NOT NULL, phone INTEGER) STRICT";
+            statement.executeUpdate(sql);
+            
+          
+
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
